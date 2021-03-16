4322bdf3bcfef739ab7c231435151a50d9b2db91
潘志豪 <zhihaopcq@travelsky.com>
2021-03-16 16:25:42 +0800

    第一次推送

---
+++ src/main/java/com/example/hello/Hello.java
@@ -0,0 +1,21 @@
+package com.example.hello;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName Hello
+ * @Deacription TODO
+ * @Author zhihaop
+ * @Date 2021/3/9 13:47
+ **/
+@RestController
+@RequestMapping("/h")
+public class Hello {
+    @GetMapping
+    public String helloWorld(){
+        return "hi";
+    }
+}
